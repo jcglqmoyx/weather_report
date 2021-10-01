5ce059a851eeb4cebc851b6497b53b3d1e9e3d49
Chintsai Hwo <jcglqmoyx@gmail.com>
2021-10-01 19:17:14 +0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import datetime
-import json
+import logging
 from datetime import datetime
 
 import requests
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+
+import json
 
 app = Flask(__name__)
 config = json.load(open('json/config.json', 'r'))
 last_call_time = datetime.fromisoformat('2008-10-10').timestamp()
 http_response = None
+cnt = 0
 
 
 @app.route('/')
@@ -23,6 +26,11 @@ def weather():
         http_response = requests.get(url)
         last_call_time = now
     data = json.loads(http_response.text)
+    ip = request.remote_addr
+    global cnt
+    cnt += 1
+    logging.basicConfig(filename='log.txt', level=logging.INFO)
+    logging.info('第' + str(cnt) + '次调用, IP: ' + ip)
     return render_template('index.html', days=config['days'], data=data)
 
 
